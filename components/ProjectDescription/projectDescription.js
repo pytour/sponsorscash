9796b0e5fe55cdce318d3363b2eca4ae9891ca42
bappyrahaman <bappyrahaman@gmail.com>
2021-03-24 03:34:52 +0600

    bug fixed of word not breaking on modal

--- components/ProjectDescription/projectDescription.js
+++ components/ProjectDescription/projectDescription.js
@@ -274,20 +274,23 @@ const projectDescription = (props) => {
 
       {modal ? (
         <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-4 mx-auto max-w-4xl">
-              {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/}
+            <div
+                className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+
+                <div className="relative w-full  my-4 mx-auto max-w-3xl">
+                    {/*content*/}
+                    <div
+                        className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                    {/*header*/}
                 <div className="flex items-center justify-center p-3 border-b border-solid border-gray-300 rounded-t">
-                  <h4 className="text-3xl text-center font-semibold">
+                  <h4 className="text-2xl text-center font-semibold">
                     {receivingAddress ? "BCH Cash Address" : "Donate"}
                   </h4>
                   <button
                     onClick={handleModlaClose}
                     className="p-1 ml-auto bg-transparent border-0 text-red  float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                   >
-                    <span className="bg-transparent text-black font-black  h-6 w-6 text-2xl block outline-none focus:outline-none">
+                    <span className="bg-transparent text-black font-black  h-6 w-6 text-3xl block outline-none focus:outline-none">
                       ×
                     </span>
                   </button>
@@ -366,7 +369,7 @@ const projectDescription = (props) => {
                     {/*footer*/}
                     <div className="flex items-center justify-center p-6 border-t border-solid border-gray-300 rounded-b">
                       <button
-                        className="bg-branding-color mr-4 text-white active:bg-branding-color font-bold uppercase text-sm px-12 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mb-1"
+                        className="bg-branding-color mr-4 text-white active:bg-branding-color font-bold uppercase text-sm md:px-12 px-6 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mb-1"
                         type="submit"
                         style={{ transition: "all .15s ease" }}
                       >
@@ -386,18 +389,17 @@ const projectDescription = (props) => {
 
                 {receivingAddress && (
                   <div>
-                    <div className=" ">
-                      <div className="flex">
+
+                      <div className="p-6 flex-auto text-center">
                         <div className=" md:my-8 items-center justify-items-center">
-                          <p className="text-center text-xl font-black px-4 pt-8 pb-6 text-site-theme">
+                          <p className=" text-xl font-black px-4 pt-8 pb-6 text-site-theme">
                             {" "}
                             Please sentd BCH to this Cash Address:{" "}
                           </p>
 
-                          <h1 className="mx-6 text-center text-xl text-black font-black p-4">
-                            {" "}
-                            {receivingAddress}{" "}
-                          </h1>
+                          <p className="mx-4 text-xl text-black font-black p-4 break-words">
+                            {receivingAddress}
+                          </p>
 
                           <p className="py-6 mx-4 text-base font-medium text-red-500">
                             Please note,this address is valid for 5 minutes,try
@@ -421,7 +423,6 @@ const projectDescription = (props) => {
                           </button>
                         </CopyToClipboard>
                       </div>
-                    </div>
                   </div>
                 )}
               </div>
