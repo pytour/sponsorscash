4feab184546490132419446c962ce4428838feeb
pytour <freebtcbot@gmail.com>
2021-03-25 01:23:57 +0400

    contactus fix

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # fundmecash
+
+
+Add .env file
+
+Edit package.json
+
+`pm2 start "npm run start-fundme" --time --name "fundme"`
--- pages/contactUs.js
+++ pages/contactUs.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { withRedux } from "../lib/redux";
 import Layout from '../components/Layout/Layout'
-import ContactForm from '../components/ContactForm/ContactForm'
+//import ContactForm from '../components/ContactForm/ContactForm'
+
+// Legacy code
+// Complete or remove
 const contactUs = () => {
     return (
         <Layout>
-            <ContactForm/>
+            {/* <ContactForm/> */}
         </Layout>
     );
 };
