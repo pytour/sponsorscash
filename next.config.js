a51b4eccf1b31a8bcb9bc71a316ca01900284985
Farhan Farooq <farhan.farooq@live.com>
2021-03-27 23:47:17 +0500

    cfg updated

--- next.config.js
+++ next.config.js
@@ -1,12 +1,31 @@
+const UglifyEsPlugin = require("uglify-es-webpack-plugin");
+
 module.exports = {
-    publicRuntimeConfig: {
-        // Will be available on both server and client
-       // https://sponsor-cash.herokuapp.com/
-        APP_URL: process.env.APP_URL,
-        FEE_AMOUNT: process.env.FEE_AMOUNT,
-        MINIMUM_AMOUNT:0.0002
-    },
-    images: {
-        domains: ['localhost','fundme.cash', '54.244.63.208'],
-    },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    // https://sponsor-cash.herokuapp.com/
+    APP_URL: process.env.APP_URL,
+    FEE_AMOUNT: process.env.FEE_AMOUNT,
+    MINIMUM_AMOUNT: 0.0002,
+  },
+  images: {
+    domains: ["localhost", "fundme.cash", "54.244.63.208"],
+  },
+  plugins: [
+    new UglifyEsPlugin({
+      mangle: {
+        reserved: [
+          "Buffer",
+          "BigInteger",
+          "Point",
+          "ECPubKey",
+          "ECKey",
+          "sha512_asm",
+          "asm",
+          "ECPair",
+          "HDNode",
+        ],
+      },
+    }),
+  ],
 };
--- server.js
+++ server.js
@@ -1,28 +1,31 @@
 // server.js
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
 const port = process.env.PORT || 3000;
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== "production";
+const cors = require("cors");
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.use(cors());
 
 app.prepare().then(() => {
   createServer((req, res) => {
     // Be sure to pass `true` as the second argument to `url.parse`.
     // This tells it to parse the query portion of the URL.
-    const parsedUrl = parse(req.url, true)
-    const { pathname, query } = parsedUrl
+    const parsedUrl = parse(req.url, true);
+    const { pathname, query } = parsedUrl;
 
-    if (pathname === '/a') {
-      app.render(req, res, '/b', query)
-    } else if (pathname === '/b') {
-      app.render(req, res, '/a', query)
+    if (pathname === "/a") {
+      app.render(req, res, "/b", query);
+    } else if (pathname === "/b") {
+      app.render(req, res, "/a", query);
     } else {
-      handle(req, res, parsedUrl)
+      handle(req, res, parsedUrl);
     }
-  }).listen(port, err => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
-  })
-})
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log("> Ready on http://localhost:3000");
+  });
+});
