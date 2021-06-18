beaf30722fd2b847b429a11b0500ff54a8c75515
pytour <freebtcbot@gmail.com>
2021-06-18 12:53:23 +0400

    upd

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     publicRuntimeConfig: {
         // Will be available on both server and client
-        // https://sponsor-cash.herokuapp.com/
         API_URL: process.env.API_URL,
         ADS_SERVER_URL: process.env.ADS_SERVER_URL
             ? process.env.ADS_SERVER_URL
@@ -11,10 +10,7 @@ module.exports = {
         domains: [
             'localhost',
             'fundme.cash',
-            'devbitcoin.cash',
-            '54.244.63.208',
-            '34.212.40.180',
-            '142.93.2.130'
+            'devbitcoin.cash'
         ]
     },
     webpack: function(config) {
